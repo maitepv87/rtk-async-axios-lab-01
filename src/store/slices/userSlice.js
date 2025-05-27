bb60fff4bb5948b98b2d5ecf35b6f1be98511513
Maite Perez Vento <maitepv87@gmail.com>
2025-05-27 11:28:34 -0400

    user/fetch createAsyncThunk implemented

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,19 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
 
 const initialState = {
   data: [],
+  isLoading: false,
+  error: null,
 };
 
 export const usersSlice = createSlice({
   name: "users",
   initialState,
-  reducers: {
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data.push(action.payload);
+    });
+
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
   },
 });
 
-export const { incrementByAmount } = usersSlice.actions;
+// export const {} = usersSlice.actions;
 
 export default usersSlice.reducer;
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axio";
+import axios from "axios";
 
 export const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
